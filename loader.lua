a88475c182b833666bad3db4c1da7b45bc93e70d
XUANTHANH <riptungbmt@gmail.com>
2026-04-27 16:09:07 +0700

    Update loader.lua
    Load UI and execute game-specific scripts based on PlaceId.

--- loader.lua
+++ loader.lua
@@ -1,1 +1,20 @@
+-- loader.lua
+local baseUrl = "https://raw.githubusercontent.com/TenBan/MyHub/main/"
 
+-- Load UI
+local UI = loadstring(game:HttpGet(baseUrl .. "ui/library.lua"))()
+
+-- Detect game và load đúng script
+local gameScripts = {
+    [2753915549] = "games/bloxfruits.lua",  -- Blox Fruits
+    [1537690962] = "games/petsim.lua",       -- Pet Sim
+}
+
+local placeId = game.PlaceId
+local scriptPath = gameScripts[placeId]
+
+if scriptPath then
+    loadstring(game:HttpGet(baseUrl .. scriptPath))()
+else
+    UI:Notify("Game này chưa được hỗ trợ!")
+end
